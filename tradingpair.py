1b29cbaef44cdf23ff6bf9e00e0480be82ee6730
Mészáros Gergely <monk@geotronic.hu>
2018-04-05 14:49:15 +0200

    minor bugfix

--- tradingpair.py
+++ tradingpair.py
@@ -10,8 +10,9 @@ class TradingPair(object):
     def __init__ (self, first, second=None ):
         if second is None: 
             if isinstance(first,TradingPair) :
-                first
-            elif: isinstance(first,str) : 
+                self.first = first.first
+                self.second = first.second
+            elif isinstance(first,str) : 
                 self.from_ticker(first)
             else:
                 raise Exception( "Must be currency pair or str")
@@ -19,7 +20,7 @@ class TradingPair(object):
             if type(first) is str : first = Currency(first)
             if type(second) is str : second = Currency(second)
             if not isinstance( first, Currency) or not isinstance(second,Currency): 
-                raise Exception("Pair member must be Currency or str not (%s,%s)"%(type(first),type(second)))
+                raise TypeError("Pair member must be Currency or str not (%s,%s)"%(type(first),type(second)))
             self.first = first
             self.second = second
 
@@ -43,6 +44,8 @@ if __name__== "__main__":
     t = TradingPair("BtC/uSD")
     assert t.ticker() == "BTC/USD" 
     assert t.url_form() == "btcusd" 
+
+    assert TradingPair('btc','eth').url_form() == "btceth"
 
     a = TradingPair(Currency('btc'), Currency('Eth'))
     assert a.url_form() == "btceth" 
